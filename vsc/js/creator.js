6c1961ae5c469be824a2d60ee051d3e531018193
Matt <109542770+Matyi24@users.noreply.github.com>
2026-09-01 18:58:00 +0200

    V1
    Sigma :3

---
+++ vsc/js/creator.js
@@ -0,0 +1,28 @@
+const container = document.querySelector('.container');
+
+container.insertAdjacentHTML('afterbegin', `
+  <nav class="navbar">
+    <div class="logo">TutorConnect</div>
+    <ul class="nav-links">
+      <li>Főoldal</li>
+      <li>Hogyan működik</li>
+      <li>Oktatók</li>
+      <li>GYIK</li>
+    </ul>
+    <div class="cta-buttons">
+      <button class="login">Belépés</button>
+      <button class="register">Regisztráció</button>
+    </div>
+  </nav>
+`);
+
+container.insertAdjacentHTML('beforeend', `
+  <footer class="footer">
+    <ul>
+      <li>Kapcsolat</li>
+      <li>GYIK</li>
+      <li>Adatvédelem</li>
+      <li>Felhasználási feltételek</li>
+    </ul>
+  </footer>
+`);
