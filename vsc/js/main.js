993cb3b28838b34e7ef773b1619bb4a1ea882595
Pelion0 <marianmate8@gmail.com>
2026-09-02 12:53:24 +0200

    indexet el lehet érni javascriptel
    index főoldal hozzé lett adva a szerverhez javascript segitségével.

--- vsc/js/main.js
+++ vsc/js/main.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const app = express()
+const path = require("path");
+
+app.use("/css", express.static(path.join(__dirname, "../css")));
+app.use("/js", express.static(path.join(__dirname, "../js")));
+app.use("/html", express.static(path.join(__dirname, "../html")));
 
 app.get('/', (req, res) => {
     res.send('hello world');
 });
 
 
+
+app.get("/index", (req, res) => {
+    res.sendFile(path.join(__dirname, "../html/index.html"));
+});
 app.listen(3000, () => console.log('Listening on port 3000...'));
