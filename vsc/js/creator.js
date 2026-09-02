e7bb3d6832b74ae349becfe47cec8dbdf036f111
Aggrroo <zsombiharcos@gmail.com>
2026-09-02 09:13:38 +0200

    Subject oldal
    tantárgyak felsorakoztatása

--- vsc/js/creator.js
+++ vsc/js/creator.js
@@ -4,7 +4,7 @@ container.insertAdjacentHTML('afterbegin', `
   <nav class="navbar">
     <div class="logo">TutorConnect</div>
     <ul class="nav-links">
-      <li>Főoldal</li>
+      <a href="/vsc/html/index.html"><li>Főoldal</li></a>
       <li>Hogyan működik</li>
       <li>Oktatók</li>
       <li>GYIK</li>
