9acf8f46dbb51f834a9b751acacf159e4f6a3860
Aggrroo <zsombiharcos@gmail.com>
2026-09-11 11:25:37 +0200

    footer fix
    lent marad már az alján

--- vsc/js/creator.js
+++ vsc/js/creator.js
@@ -1,28 +1,55 @@
-const container = document.querySelector('.container');
+const body = document.body;
 
-container.insertAdjacentHTML('afterbegin', `
-  <nav class="navbar">
-   <a href="index"> <div class="logo">TutorConnect</div></a>
-    <ul class="nav-links">
-      <a href="index"><li>Főoldal</li></a>
-      <li>Oktatók</li>
-      <a href="subs"><li>Tantárgyak</li></a>
-      <a href="gyik"><li>GYIK</li></a>
-    </ul>
-    <div class="cta-buttons">
-      <a href="login"><button class="login">Belépés</button></a>
-      <a href="register"><button class="register">Regisztráció</button></a>
-    </div>
-  </nav>
+// =========================
+// NAVBAR
+// =========================
+
+body.insertAdjacentHTML('afterbegin', `
+    <nav class="navbar">
+        <a href="index">
+            <div class="logo">TutorConnect</div>
+        </a>
+
+        <ul class="nav-links">
+            <a href="index">
+                <li>Főoldal</li>
+            </a>
+
+            <li>Oktatók</li>
+
+            <a href="subs">
+                <li>Tantárgyak</li>
+            </a>
+
+            <a href="gyik">
+                <li>GYIK</li>
+            </a>
+        </ul>
+
+        <div class="cta-buttons">
+            <a href="login">
+                <button class="login">Belépés</button>
+            </a>
+
+            <a href="register">
+                <button class="register">Regisztráció</button>
+            </a>
+        </div>
+    </nav>
 `);
 
-container.insertAdjacentHTML('beforeend', `
-  <footer class="footer">
-    <ul>
-      <li>Kapcsolat</li>
-      <li>GYIK</li>
-      <li>Adatvédelem</li>
-      <li>Felhasználási feltételek</li>
-    </ul>
-  </footer>
+
+// =========================
+// FOOTER
+// =========================
+
+body.insertAdjacentHTML('beforeend', `
+    <footer class="footer">
+        <ul>
+            <li>Kapcsolat</li>
+            <li>GYIK</li>
+            <li>Adatvédelem</li>
+            <li>Felhasználási feltételek</li>
+        </ul>
+    </footer>
 `);
